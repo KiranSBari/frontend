cf00cd6732d6f215cf7c469e7b8392ac94605dad
KiranSBari <101690328+KiranSBari@users.noreply.github.com>
2022-04-08 17:35:19 +0530

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BaseModule } from './base/base.module';
+import { ContainerComponent } from './base/container/container.component';
 import { HederComponent } from './heder/heder.component';
 
 @NgModule({
@@ -10,7 +12,8 @@ import { HederComponent } from './heder/heder.component';
     HederComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
